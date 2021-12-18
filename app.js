4e2329a7585de4f10242ffc9bd306d216febb00f
yukai1023 <wuyukai1023work@gmail.com>
2021-12-18 18:26:46 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
 const app = express()
 const Todo = require('./models/todo') // 載入 Todo model
+// 引用 body-parser
+const bodyParser = require('body-parser')
 
 mongoose.connect('mongodb://localhost/todo-list') // 設定連線到 mongoDB
 
@@ -20,6 +22,8 @@ db.once('open', () => {
 //hbs
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 // 設定首頁路由
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
@@ -28,7 +32,18 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error)) // 錯誤處理
 })
 
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})  
 // 設定 port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
+
